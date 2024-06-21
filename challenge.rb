6d5f8a01f54afa695fdbd9470f754fd1b32674f8
Austin Smith <austin@asmithdev.com>
2024-06-21 10:00:26 -0400

    Optimize ruby further

--- challenge.rb
+++ challenge.rb
@@ -9,18 +9,20 @@ class Station
     @sum = 0
     @max = -Float::INFINITY
     @count = 0
+    @mutex = Mutex.new
   end
 
   def add_temperature(temp)
-    @min = temp if temp < @min
-    @max = temp if temp > @max
-    @sum += temp
-    @count += 1
+    @mutex.synchronize do
+      @min = temp if temp < @min
+      @max = temp if temp > @max
+      @sum += temp
+      @count += 1
+    end
   end
 
   def average_temperature
-    return 0 if @count == 0 # avoid division by zero
-    @sum / @count
+    @count == 0 ? 0 : @sum / @count
   end
 end
 
@@ -52,11 +54,8 @@ def parse_line(line)
   parts = line.split(';')
   station = parts[0]
   temperature = parts[1].to_f
-  return station, temperature
+  [station, temperature]
 end
-
-buffer = Concurrent::Array.new
-buffer_size = 5 * (4 * 250000) # Adjust the buffer size as needed
 
 def process_subchunk(subchunk, stations)
   subchunk.each do |line|
@@ -77,23 +76,25 @@ def process_chunk(chunk, stations, subchunk_size, progress_tracker)
   progress_tracker.print_progress
 end
 
-def process_data(filename, stations, buffer, buffer_size, subchunk_size, total_lines, progress_tracker)
+def process_data(filename, stations, buffer_size, subchunk_size, total_lines, progress_tracker)
   start_time = Time.now
 
   pool = Concurrent::FixedThreadPool.new(8) # Adjust the number of threads as needed
 
+  buffer = []
+
   File.foreach(filename) do |line|
     buffer << line
 
-    if buffer.length >= buffer_size && buffer.length % buffer_size == 0
+    if buffer.length >= buffer_size
       chunk_to_process = buffer.shift(buffer_size) # Remove processed lines from buffer immediately
       pool.post { process_chunk(chunk_to_process, stations, subchunk_size, progress_tracker) } # Process the chunk in parallel
     end
   end
 
   # Process any remaining lines in the buffer
-  while buffer.length > 0
-    chunk_to_process = buffer.shift(buffer_size) # Remove processed lines from buffer immediately
+  unless buffer.empty?
+    chunk_to_process = buffer.shift(buffer.size) # Remove processed lines from buffer immediately
     pool.post { process_chunk(chunk_to_process, stations, subchunk_size, progress_tracker) } # Process the chunk in parallel
   end
 
@@ -119,5 +120,6 @@ def print_data(stations)
 end
 
 # Replace 'measurements.txt' with your actual filename
+buffer_size = 10 * (4 * 250000) # Adjust the buffer size as needed
 subchunk_size = buffer_size / 8 # Size of each subchunk
-process_data('measurements.txt', stations, buffer, buffer_size, subchunk_size, total_lines, progress_tracker)
+process_data('measurements.txt', stations, buffer_size, subchunk_size, total_lines, progress_tracker)
